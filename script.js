25bc2e25ae78c361060b7ea7963b74e75fa9f9c1
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-01-01 14:49:26 +0100

    binding events moved to a separate function

--- script.js
+++ script.js
@@ -28,22 +28,7 @@
         render();
     };
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}
-            >
-                <button class="js-done">zrobione?</button>
-                <button class="js-remove">usuń</button>
-                ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -61,6 +46,25 @@
         });
     };
 
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}
+            >
+                <button class="js-done">zrobione?</button>
+                <button class="js-remove">usuń</button>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+    };
+
     const onFormSubmit = (event) => {
         event.preventDefault();
 
@@ -72,7 +76,7 @@
         }
 
         addNewTask(newTaskContent);
-    }
+    };
 
     const init = () => {
         render();
